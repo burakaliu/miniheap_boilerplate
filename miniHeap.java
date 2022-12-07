f9980d249c6fd5fe0e3b0638870ccb9f5ca6702a
Burak Unlu <burakaliunlu@gmail.com>
2022-12-06 19:17:26 -0500

    added some methods but did not finish

--- miniHeap.java
+++ miniHeap.java
@@ -1,3 +1,5 @@
+import java.text.ParseException;
+
 import java.util.ArrayList;
 
 /*
@@ -8,40 +10,71 @@ import java.util.ArrayList;
 
 public class miniHeap {
 
-    //static int heap[];
-    static ArrayList<Integer> heap = new ArrayList<Integer>();
-    private int size;
-    private int max;
+    static private int heap[];
+    static private int size;
+    static private int index;
 
     public miniHeap(int max){
-        this.max = max;
-        this.size = 0;
-
-        //heap = new int[this.max + 1];
+        this.size = max;
+        this.index = 0;
+        heap = new int[this.size];
     }
 
-    private int parent(int position){
-        return position/2;
+    private static int parent(int position){ //get parent node
+        return (position-1)/2;
     }
-    private int left(int pos){
-        return (2*pos);
-    }
-    private int right(int pos){
+    private static int left(int pos){ //get left child node
         return (2*pos)+1;
     }
-    private void swap(int finalpos, int firstpos){
-        int temp = heap.get(finalpos);
-        heap.set(finalpos, firstpos);
-        heap.set(firstpos, temp);
+    private static int right(int pos){ //get right child node
+        return (2*pos)+2;
+    }
+    private static void swap(int finalpos, int firstpos){ //swap a node from first pos to final pos 
+        int temp = heap[finalpos];
+        heap[finalpos] = heap[firstpos];
+        heap[firstpos] = temp;
+    }
+    private static boolean isLeaf(int i) { //check wether a node is a leaf
+		if (right(i) >= size || left(i) >= size) {
+			return true;
+		}
+		return false;
+	}
+    private static void heapify(int i){ //sift the heap into a proper min heap
+        if (!isLeaf(i)) { //not a leaf
+			if (heap[i] > heap[left(i)] || heap[i] > heap[right(i)]) { //right or left child node is smaller than the parent
+				if (heap[left(i)] < heap[right(i)]) { //if left child is less than right child
+					swap(i, left(i));
+					heapify(left(i));
+				} else { 
+					swap(i, right(i));
+					heapify(right(i));
+				}
+			}
+		}
     }
     
     public static void insert(int insertMe){
-        heap.add(insertMe);
+        //if no more space available
+        if (index >= size) {
+			return;
+		}
+        heap[index] = insertMe;
+        int curr = index;
+
+        while (heap[curr] < heap[parent(curr)]){
+            swap(parent(curr), curr);
+            curr = parent(curr);
+        }
+        index++;
     }
     public static int delete() {
-        return heap.remove(0);
+        int popped = heap[0];
+		heap[0] = heap[--index];
+		heapify(0);
+		return popped;
     }
     public static int peek(){
-        return heap.get(0);
+        return heap[0];
     }
 }
